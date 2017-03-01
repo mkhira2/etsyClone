bc1e73ece0ce43b8677d60485c963f42a68aba36
Kenji Hirabayashi <mkhira2@gmail.com>
2017-03-01 17:27:49 -0600

    backbone loaded

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// base url: https://openapi.etsy.com/v2/listings/active
+// api key: 9sfgsyb7qbqbo1c79sraetsl
+// format: https://openapi.etsy.com/v2/listings/active?api_key=9sfgsyb7qbqbo1c79sraetsl
+// search url: https://openapi.etsy.com/v2/listings/active?keywords=teeth&api_key=9sfgsyb7qbqbo1c79sraetsl
+
+console.log(Backbone)
+
+var Router = Backbone.Router.extend({
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var instance = new Router()
+
+Backbone.history.start()
