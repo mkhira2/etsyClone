d02a9c28cbe44bd5c5447a368ac4b044c6afc647
Kenji Hirabayashi <mkhira2@gmail.com>
2017-03-02 09:31:25 -0600

    base html in place

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-// base url: https://openapi.etsy.com/v2/listings/active
-// api key: 9sfgsyb7qbqbo1c79sraetsl
 // format: https://openapi.etsy.com/v2/listings/active?api_key=9sfgsyb7qbqbo1c79sraetsl
 // search url: https://openapi.etsy.com/v2/listings/active?keywords=teeth&api_key=9sfgsyb7qbqbo1c79sraetsl
 
+var baseUrl = 'https://openapi.etsy.com/v2/listings/active.js?api_key='
+var apiKey = '9sfgsyb7qbqbo1c79sraetsl'
 console.log(Backbone)
 
 var Router = Backbone.Router.extend({
-
+	routes: {
+		"home": "#homePage#",
+		"search/:query": "#searchPage#",
+		"details/:id": "#detailsPage#",
+		"*default": "#goHome#"
+	}
 })
 
+var instance = new Router()
+
+Backbone.history.start()
 
 
 
