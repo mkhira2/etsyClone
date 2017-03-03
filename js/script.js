9c0e2fe81bf2918b2724e9684862e1afe454261c
Kenji Hirabayashi <mkhira2@gmail.com>
2017-03-03 02:32:13 -0600

    images loaded, search feature added

--- js/script.js
+++ js/script.js
@@ -1,54 +1,124 @@
-// format: https://openapi.etsy.com/v2/listings/active?api_key=9sfgsyb7qbqbo1c79sraetsl
-// search url: https://openapi.etsy.com/v2/listings/active?keywords=teeth&api_key=9sfgsyb7qbqbo1c79sraetsl
+//--------------------------------------------------
+// REFERENCE
+//--------------------------------------------------
 
-var baseUrl = 'https://openapi.etsy.com/v2/listings/active.js?api_key='
+var baseUrl = 'https://openapi.etsy.com/v2/listings/active.js'
 var apiKey = '9sfgsyb7qbqbo1c79sraetsl'
-console.log(Backbone)
+    // format: https://openapi.etsy.com/v2/listings/active?api_key=9sfgsyb7qbqbo1c79sraetsl
+    // search url: https://openapi.etsy.com/v2/listings/active?keywords=teeth&api_key=9sfgsyb7qbqbo1c79sraetsl
 
-var Router = Backbone.Router.extend({
-	routes: {
-		"home": "#homePage#",
-		"search/:query": "#searchPage#",
-		"details/:id": "#detailsPage#",
-		"*default": "#goHome#"
-	}
+//--------------------------------------------------
+// MODELS
+//--------------------------------------------------
+
+var MultiCollection = Backbone.Collection.extend({
+    url: 'https://openapi.etsy.com/v2/listings/active.js',
+
+    parse: function(apiResponse) {
+        return apiResponse.results
+    }
+
 })
 
-var instance = new Router()
+var SingleModel = Backbone.Model.extend({
 
-Backbone.history.start()
+})
 
+//--------------------------------------------------
+// VIEWS
+//--------------------------------------------------
 
+var MultiView = Backbone.View.extend({
+    initialize: function() {
+        document.querySelector('.container').innerHTML = '<img src="magnify.gif">'
+        this.listenTo(this.collection, 'sync', this.render)
+    },
 
+    render: function() {
+        var containerNode = document.querySelector('.container')
+        var htmlString = ''
+        this.collection.forEach(function(inputModel) {
+            htmlString += '<div class="itemContainer"' + inputModel.get('listing_id') + '">'
+             			+ '<div class="title">' + inputModel.get('title') + '</div>'
+             			+ '<img src="' + inputModel.get('Images')[0].url_170x135 + '">'
+             			+ '<div class="price">' + '$' + inputModel.get('price') + '</div>'
+             			+ '</div>'
+            containerNode.innerHTML = htmlString
+        })
+    }
+})
 
+var SingleView = Backbone.View.extend({
 
+})
 
+//--------------------------------------------------
+// CONTROLLER
+//--------------------------------------------------
 
+var EtsyRouter = Backbone.Router.extend({
+    routes: {
+        "home": "showMultiView",
+        "search/:query": "performSearch",
+        "details/:id": "showSingleView",
+        "*default": "takeMeHome"
+    },
 
+    showMultiView: function() {
+        var homeInstance = new MultiCollection()
+        homeInstance.fetch({
+            dataType: 'jsonp',
+            data: {
+                includes: "Images",
+                "api_key": '9sfgsyb7qbqbo1c79sraetsl'
+            }
+        })
+        new MultiView({
+            collection: homeInstance
+        })
+    },
 
+    performSearch: function(query) {
+        var newSearch = new MultiCollection()
+        newSearch.fetch({
+            dataType: 'jsonp',
+            data: {
+                includes: "Images",
+                "api_key": "9sfgsyb7qbqbo1c79sraetsl",
+                keywords: query
+            }
+        })
+        new MultiView({
+            collection: newSearch
+        })
+    },
 
+    showSingleView: function(id) {
+        
+    },
 
+    takeMeHome: function() {
+    	location.hash = 'home'
+    }
+})
 
+//--------------------------------------------------
+// SEARCH
+//--------------------------------------------------
 
+var searchNode = document.querySelector('.search')
+searchNode.addEventListener('keydown', function(eventObj) {
+        if (eventObj.keyCode === 13) {
+            var input = eventObj.target.value 
+            location.hash = 'search/' + input 
+            eventObj.target.value = ''
+        }
+    })
 
+//--------------------------------------------------
+// START POINT
+//--------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var instance = new Router()
+new EtsyRouter()
 
 Backbone.history.start()
